cdd5c85dea07cc8ef5e45c96dd284b25ed4bd50f
viktoras911 <viktoras911@gmail.com>
2026-08-14 20:59:52 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,10 @@ android {
 
     defaultConfig {
         applicationId = "com.example.pricescanner"
+
         minSdk = 24
         targetSdk = 36
+
         versionCode = 2
         versionName = "2.0"
 
@@ -20,6 +22,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -31,25 +34,34 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+}
 
-    kotlinOptions {
-        jvmTarget = "17"
+kotlin {
+    compilerOptions {
+        jvmTarget.set(
+            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        )
     }
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
+    // CameraX
     val cameraVersion = "1.6.1"
+
     implementation("androidx.camera:camera-core:$cameraVersion")
     implementation("androidx.camera:camera-camera2:$cameraVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraVersion")
     implementation("androidx.camera:camera-view:$cameraVersion")
 
+    // Google ML Kit OCR
     implementation("com.google.mlkit:text-recognition:16.0.1")
 
+    // Unit tests
     testImplementation("junit:junit:4.13.2")
 }
